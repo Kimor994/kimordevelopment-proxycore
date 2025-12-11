a816ded36658dfacd79c2c353a2c57c04881a66d
Kimor994 <kimorjestpog@gmail.com>
2025-12-12 00:35:40 +0100

    .

--- src/main/java/pl/kimor/development/updater/Updater.java
+++ src/main/java/pl/kimor/development/updater/Updater.java
@@ -1,4 +1,21 @@
 package pl.kimor.development.updater;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Updater {
+
+    private final URL updateCheckUrl = new URL("https://api.github.com/repos/Kimor994/kimordevelopment-proxycore/releases/latest");
+
+    public Updater() throws MalformedURLException {
+    }
+
 }
